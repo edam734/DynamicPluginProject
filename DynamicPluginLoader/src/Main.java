5f3e948c5be186949fab7d74bbb8422be42d4de6
Eduardo <edam734@gmail.com>
2026-09-22 23:22:31 +0100

    feat: discover plugin JARs and validate plugin classes.

--- DynamicPluginLoader/src/Main.java
+++ DynamicPluginLoader/src/Main.java
@@ -1,8 +1,16 @@
+import com.edam.pluginapi.Plugin;
+
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,12 +26,63 @@ public class Main {
         try (Stream<Path> stream = Files.list(pathToDir)) {
             return stream.filter(Files::isRegularFile)
                     .filter(file -> file.toString().toLowerCase().endsWith(".jar"))
-                    .filter(Main::verifyJar)
+                    .filter(path -> {
+                        try {
+                            return verifyJar(path);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
                     .collect(Collectors.toSet());
         }
     }
 
-    private static boolean verifyJar(Path jar) {
-        return true; // TODO
+    private static boolean verifyJar(Path jarPath) throws IOException {
+        boolean isValid = false;
+        try (JarFile jar = new JarFile(jarPath.toString())) {
+            Manifest manifest = jar.getManifest();
+            String pluginClassName = manifest.getMainAttributes().getValue("Plugin-Class");
+
+
+            URL jarUrl = jarPath.toUri().toURL();
+            Class<?> pluginClass = getPluginClass(jarUrl, pluginClassName);
+            // implements the class com.edam.pluginapi.Plugin
+            if (Plugin.class.isAssignableFrom(pluginClass)) {
+                // checks if this class is instantiable
+                isValid = isInstantiable(pluginClass);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return isValid;
+    }
+
+    private static Class<?> getPluginClass(URL jarUrl, String className) throws IOException,
+            ClassNotFoundException {
+        try (URLClassLoader classLoader = new URLClassLoader(new URL[]{jarUrl},
+                Thread.currentThread().getContextClassLoader())) {
+            return Class.forName(className, false, classLoader);
+        }
+    }
+
+    private static boolean isInstantiable(Class<?> clazz) {
+        // 1. Basic type validations
+        if (clazz.isInterface() || clazz.isEnum() || clazz.isAnnotation()) {
+            return false;
+        }
+        // 2. Checks if the class is abstract
+        int modifier = clazz.getModifiers();
+        if (Modifier.isAbstract(modifier)) {
+            return false;
+        }
+
+        try {
+            // Attempts to get the empty constructor (without parameters)
+            Constructor<?> constructor = clazz.getConstructor();
+            // Ensures that the constructor is indeed public
+            return Modifier.isPublic(constructor.getModifiers());
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
     }
 }
